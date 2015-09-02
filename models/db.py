fe61377c31d2777e6aa7fe069d85c62dbbf19fa8
Juan Diego Romero González <jd@inkatze.me>
2015-09-02 13:34:30 -0500

    Base for the new version.

--- models/db.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-db = DAL('sqlite://words.db')
-
-db.define_table('word', Field('word', 'string', notnull=True, required=True), migrate=False)
---
+++ random.js
@@ -0,0 +1,26 @@
+var dictionary;
+$.ajax({
+    url: "spanish.json",
+    dataType: "json",
+    isLocal: true,
+    crossDomain: true,
+    async: false,
+    global: false,
+    success: function(data){
+        dictionary = data;
+    },
+});
+
+function getRandomWord() {
+    var rand = dictionary[Math.floor(Math.random() * dictionary.length)];
+    return rand;
+}
+
+function changeWord() {
+    $("#word").hide(0, function() {
+        $(this).html(getRandomWord());
+    }).fadeIn(600);
+    setTimeout("changeWord()", 6000);
+}
+
+changeWord();
